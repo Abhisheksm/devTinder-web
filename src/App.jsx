a9794d9bce6b78a34eb66852fde4a913fdc3535f
Abhishek Mundada <abhishekmundada3@gmail.com>
2025-03-16 23:53:09 +0530

    Configured daisyui and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-     Hello World
-    </h1>
+    <>
+    <BrowserRouter basename='/'>
+    <Routes>
+      <Route path='/' element={<Body/>}>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+    
   )
 }
 
