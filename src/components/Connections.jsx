0ad6b319d550d32ea6315e41388298b783b84198
Abhishek Mundada <abhishekmundada3@gmail.com>
2025-03-29 17:17:29 +0530

    Implemented basic review request and show connection list feature UI part-04

---
+++ src/components/Connections.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addConnections } from '../store/connectionSlice'
+
+const Connections = () => {
+    const dispatch = useDispatch()
+    const connections = useSelector(state => state?.connections)
+
+    const fetchConnections = async () => {
+        try {
+            const data = await axios.get(BASE_URL + '/user/connections', { withCredentials: true })
+            dispatch(addConnections(data?.data?.data))
+        }
+        catch (err) {
+            console.error('err')
+        }
+    }
+
+    useEffect(() => {
+        fetchConnections()
+    }, [])
+
+    if (!connections) return
+
+    if (!connections?.length) return <h1 className='text-center my-5 text-xl'> No connections found</h1>
+
+    return (
+        <div>
+            <h1 className='text-center my-5 text-3xl'>Connections</h1>
+            <div className='flex flex-wrap items-center justify-center'>
+                {connections?.map(connection => {
+                    const { firstName, lastName, photoUrl, about, age, gender, _id } = connection
+                    return <div key={_id} className="card card-side bg-base-300 shadow-sm m-10 w-1/3">
+                        <figure>
+                            <img
+                                style={{ height: '150px', width: '150px', borderRadius: '20px' }}
+                                src={photoUrl}
+                                alt="user-photo" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{firstName}{' '}{lastName} </h2>
+                            {age && gender && <p>{age} {' '} {gender}</p>}
+                            <p>{about}</p>
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Connections
---
+++ src/components/Requests.jsx
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addRequests } from '../store/requestSlice'
+
+const Requests = () => {
+    const dispatch = useDispatch()
+    const requests = useSelector(state => state?.requests)
+
+    const fetchRequests = async () => {
+        try {
+            const data = await axios.get(BASE_URL + '/user/requests/received', { withCredentials: true })
+            dispatch(addRequests(data?.data?.data))
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
+    const reviewRequest = async (id, status) => {
+        try {
+            const data = await axios.post(`${BASE_URL}/request/review/${status}/${id}`, {}, { withCredentials: true })
+            fetchRequests()
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
+
+    useEffect(() => {
+        fetchRequests()
+    }, [])
+
+    if (!requests) return
+
+    if (!requests?.length) return <h1 className='text-center my-5 text-xl'> No requests found</h1>
+
+    return (
+        <div>
+            <h1 className='text-center my-5 text-3xl'>Requests</h1>
+            <div className='flex flex-wrap items-center justify-center'>
+                {requests?.map(request => {
+                    const { firstName, lastName, photoUrl, about, age, gender, _id } = request?.fromUserId
+                    return <div key={_id} className="card card-side bg-base-300 shadow-sm m-10 w-1/3">
+                        <figure>
+                            <img
+                                style={{ height: '150px', width: '150px', borderRadius: '20px' }}
+                                src={photoUrl}
+                                alt="user-photo" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{firstName}{' '}{lastName} </h2>
+                            {age && gender && <p>{age} {' '} {gender}</p>}
+                            <p>{about}</p>
+
+                        </div>
+                        <div className="card-actions flex flex-col justify-between m-5">
+                            <button className="btn btn-secondary my-2" onClick={() => { reviewRequest(request?._id, 'accepted',) }}>Accept</button>
+                            <button className="btn btn-primary" onClick={() => { reviewRequest(request?._id, 'rejected') }}>Reject</button>
+
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Requests
